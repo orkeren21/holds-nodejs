2a79129c592bf39b2172a1847ac62236a72f7df7
Or Keren <or.keren@wework.com>
2019-07-23 18:17:38 +0300

    serve static html

--- server.js
+++ server.js
@@ -2,21 +2,23 @@ import db from "./models";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 import { logger } from "./utils/logger";
+const express = require('express');
 
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server-express");
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ 
-    typeDefs: gql(typeDefs), 
-    resolvers,
-    context: { db } 
+const server = new ApolloServer({
+  typeDefs: gql(typeDefs),
+  resolvers,
 });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+const app = express();
+server.applyMiddleware({ app });
+
+app.use(express.static('public'));
+app.listen({ port: 4000 }, () => {
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
   logger.info(`Server ready at ${url}`);
 });
