dd9e37abfaf9ef5eb2a1d59396a45a6894d2e5ad
Or Keren <or.keren@checkmarx.com>
2020-01-28 12:46:14 +0200

    added new endpoint

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import db from "./models";
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
-import { logger } from "./utils/logger";
+import * as WishlistService from "./services/wishlistService";
+import * as WishlistEntryService from "./services/wishlistEntryService";
+
 const express = require('express');
 
 const { ApolloServer, gql } = require("apollo-server-express");
@@ -18,6 +20,29 @@ const app = express();
 server.applyMiddleware({ app });
 
 app.use(express.static('public'));
+
+app.get('/api/v1/wishlists', async (req, res) => {
+  
+  const wishlists = await WishlistService.getAllWishlists();
+  
+  res.status(200).send({
+    success: 'true',
+    message: 'retrieved successfully',
+    wishlists: wishlists,
+  })
+});
+
+app.get('/api/v1/wishlist-entries', async (req, res) => {
+  const wishlistID = req.query.wishlistID;
+  const entries = await WishlistEntryService.findEntriesByWishlistId(wishlistID);
+
+  res.status(200).send({
+    success: 'true',
+    message: 'retrieved successfully',
+    entries: entries,
+  })
+});
+
 app.listen({ port: 4000 }, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
   //logger.info(`Server ready at ${url}`);
